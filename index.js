c259f9a9d0000a9464df75ce3a601cb030385b16
adhyaya <adhyayathakur@gmail.com>
2021-01-15 23:51:37 +0530

    node first example

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+  perimeter: (x, y) => (2 * (x + y)),
+  area: (x, y) => (x * y),
+};
+
+function solveRect(l, b) {
+  console.log("solving for l=", l, "b=", b);
+
+  if (l <= 0 || b <= 0) {
+    console.log("rectangle dimensions should be greater than 0 ");
+  } else {
+    console.log("perimeter:", rect.perimeter(l, b));
+    console.log("area", rect.area(l, b));
+  }
+}
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-9, 8);
