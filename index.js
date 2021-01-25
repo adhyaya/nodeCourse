f86714ec20598a4ed30f4639f9d8c2c3a6466af4
adhyayathakur@gmail.com <adhyayathakur@gmail.com>
2021-01-25 18:10:54 +0530

    node http completed

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.headers);
-  console.log(res);
+  console.log("request for" + req.url + 'by method' + req.method);
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>Hello world</h1></body></html>')
+  if (req.method === 'GET') {
+    let fileUrl;
+    if (req.url === '/') fileUrl = '/index.html';
+    else fileUrl = req.url;
+
+    let filePath = path.resolve('./public' + fileUrl);
+    const fileExt = path.extname(filePath);
+    console.log(fileExt)
+    if (fileExt == '.html') {
+      if (fileExt == '.html') {
+        fs.exists(filePath, (exists) => {
+
+          if (!exists) {
+            res.statusCode = 404;
+            res.setHeader('Content-Type', 'text/Html');
+            res.end('<html><body><h1>Notfound</h1></body></html>');
+            return;
+          }
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/Html');
+          fs.createReadStream(filePath).pipe(res);
+        })
+      }
+      else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/Html');
+        res.end('<html><body><h1>Notfound and Not a html page</h1></body></html>');
+        return;
+      }
+    }
+    else {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/Html');
+      res.end('<html><body><h1>Notfound and Not a html page</h1></body></html>');
+      return;
+    }
+  }
+
 })
 
 server.listen(port, hostname, () => {
